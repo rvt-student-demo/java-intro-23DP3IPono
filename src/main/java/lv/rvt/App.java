2ad16e506bc6039af267a0a761fa35d2434563d7
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2024-09-06 08:19:36 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,20 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in)
+
+        System.out.println("Ievadi savu vārdu: ");
+        String name = scanner.nextLine();
+        System.out.println();
+        String lastname = scanner.nextLine();
+        System.out.println();
+        String group = scanner.nextLine();
+        System.out.println(name + lastname + group);
 
     }
 
