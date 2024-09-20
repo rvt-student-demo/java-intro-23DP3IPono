872ff10d50f5ab240f3a1d1dba416541043089f0
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2024-09-20 07:59:40 +0000

    Grades and points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -36,11 +36,28 @@ public class App
 
         
         
-        System.out.println("Give speed: ");
+        System.out.println("Give points [0-100]: ");
         int x = Integer.valueOf(scanner.nextLine());
-        if (x > 120) {
-            System.out.println("Speeding ticket!");
+        if (x < 0) {
+            System.out.println("Grade: impossible!");
+        } else if (x >= 0 && x <= 49) {
+            System.out.println("Grade: failed");
+        } else if (x >= 50 && x <= 59) {
+            System.out.println("Grade: 1");
+        } else if (x >= 60 && x <= 69) {
+            System.out.println("Grade: 2");
+        } else if (x >= 70 && x <= 79) {
+            System.out.println("Grade: 3");
+        } else if (x >= 80 && x <= 89) {
+            System.out.println("Grade: 4");
+        } else if (x >= 90 && x <= 99) {
+            System.out.println("Grade: 5");
+        } else if (x >= 100) {
+            System.out.println("Grade: incredible!");
         }
+
+
+
     
 
 
