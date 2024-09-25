b997c6fbc2130b1f7a7893ac9f04e237838f3951
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2024-09-25 07:19:10 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -36,24 +36,45 @@ public class App
 
         
         
-        System.out.println("Give points [0-100]: ");
-        int x = Integer.valueOf(scanner.nextLine());
-        if (x < 0) {
-            System.out.println("Grade: impossible!");
-        } else if (x >= 0 && x <= 49) {
-            System.out.println("Grade: failed");
-        } else if (x >= 50 && x <= 59) {
-            System.out.println("Grade: 1");
-        } else if (x >= 60 && x <= 69) {
-            System.out.println("Grade: 2");
-        } else if (x >= 70 && x <= 79) {
-            System.out.println("Grade: 3");
-        } else if (x >= 80 && x <= 89) {
-            System.out.println("Grade: 4");
-        } else if (x >= 90 && x <= 99) {
-            System.out.println("Grade: 5");
-        } else if (x >= 100) {
-            System.out.println("Grade: incredible!");
+        // System.out.println("Give points [0-100]: ");
+        // int x = Integer.valueOf(scanner.nextLine());
+        // if (x < 0) {
+        //     System.out.println("Grade: impossible!");
+        // } else if (x >= 0 && x <= 49) {
+        //     System.out.println("Grade: failed");
+        // } else if (x >= 50 && x <= 59) {
+        //     System.out.println("Grade: 1");
+        // } else if (x >= 60 && x <= 69) {
+        //     System.out.println("Grade: 2");
+        // } else if (x >= 70 && x <= 79) {
+        //     System.out.println("Grade: 3");
+        // } else if (x >= 80 && x <= 89) {
+        //     System.out.println("Grade: 4");
+        // } else if (x >= 90 && x <= 99) {
+        //     System.out.println("Grade: 5");
+        // } else if (x >= 100) {
+        //     System.out.println("Grade: incredible!");
+        // }
+
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scanner.nextLine());
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        }
+        else if (gift >= 5000 && gift <= 25000) {
+            System.out.println("Tax: " + (100+(gift-5000)*0.08));
+        }
+        else if (gift > 25000 && gift <= 55000) {
+            System.out.println("Tax: " + (1700+(gift-25000)*0.1));
+        }
+        else if (gift > 55000 && gift <= 200000) {
+            System.out.println("Tax: " + (4700+(gift-55000)*0.12));
+        }
+        else if (gift > 200000 && gift <= 1000000) {
+            System.out.println("Tax: " + (22100+(gift-200000)*0.15));
+        }
+        else {
+            System.out.println("Tax: " + (142100+(gift-1000000)*0.17));
         }
 
 
