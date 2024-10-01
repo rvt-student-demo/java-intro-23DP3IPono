797765e96f5b7d2bee6e4c488600e13dffdb6935
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2024-10-01 16:30:47 +0000

    Sum of Sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -95,11 +95,23 @@ public class App
 
 
 
-        System.out.println();
-        int number = scanner.nextInt();
-        for (int i = 0; i <= number; i++) {
-            System.out.println(i);
+        // System.out.println();
+        // int number = scanner.nextInt();
+        // for (int i = 0; i <= number; i++) {
+        //     System.out.println(i);
+        // }
+
+
+        System.out.println("Last number?");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
+
+        System.out.println("The sum is " + sum);
+        
 
 
 
