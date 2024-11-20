f749fef40880e91589ddd69630fa78602054212d
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2024-11-20 13:31:25 +0000

    Book

---
+++ src/main/java/lv/rvt/Book.java
@@ -0,0 +1,32 @@
+package lv.rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Book {
+    private String title;
+    private int pages;
+    private int publicationYear;
+
+    public Book(String title, int pages, int publicationYear) {
+        this.title = title;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getDetails() {
+        return title + ", " + pages + " pages, " + publicationYear;
+    }
+}
---
+++ src/main/java/lv/rvt/Book1.java
@@ -0,0 +1,41 @@
+package lv.rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Book1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+            System.out.print("Pages: ");
+            int pages = Integer.parseInt(scanner.nextLine());
+            System.out.print("Publication year: ");
+            int publicationYear = Integer.parseInt(scanner.nextLine());
+
+            books.add(new Book(title, pages, publicationYear));
+        }
+
+        System.out.print("What information will be printed? ");
+        String choice = scanner.nextLine();
+
+        if (choice.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book.getDetails());
+            }
+        } else if (choice.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.getTitle());
+            }
+        }
+
+        scanner.close();
+    }
+}
