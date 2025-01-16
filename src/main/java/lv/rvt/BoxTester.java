e4df7cfaca7d596d08fbb8a6741bbce8f9713df5
23DP3IPono <166009312+23DP3IPono@users.noreply.github.com>
2025-01-16 07:49:33 +0000

    Chapter 50, Exercise 4

--- src/main/java/lv/rvt/BoxTester.java
+++ src/main/java/lv/rvt/BoxTester.java
@@ -2,7 +2,7 @@ package lv.rvt;
 
 public class BoxTester {
     public static void main(String[] args) {
-        Box box = new Box(2.5, 5.0, 6.0);
+        Box box = new Box(2.0, 4.0, 6.0);
         System.out.println("Original Box:");
         System.out.println("Length: " + box.length());
         System.out.println("Height: " + box.height());
@@ -10,13 +10,19 @@ public class BoxTester {
         System.out.println("Area: " + box.area() + " Volume: " + box.volume());
 
 
-        Box box2 = new Box(box);
-        System.out.println("\nCopied Box:");
-        System.out.println("Length: " + box2.length());
-        System.out.println("Height: " + box2.height());
-        System.out.println("Width: " + box2.width());
-        System.out.println("Area: " + box2.area() + " Volume: " + box2.volume());
+        Box biggerBox = Box.biggerBox(box);
+        System.out.println("\nBigger Box:");
+        System.out.println("Length: " + biggerBox.length());
+        System.out.println("Height: " + biggerBox.height());
+        System.out.println("Width: " + biggerBox.width());
+        System.out.println("Area: " + biggerBox.area() + " Volume: " + biggerBox.volume());
 
-        System.out.println("Box == Box2: " + (box == box2));
+        
+        Box smallerBox = Box.smallerBox(box);
+        System.out.println("\nSmaller Box:");
+        System.out.println("Length: " + smallerBox.length());
+        System.out.println("Height: " + smallerBox.height());
+        System.out.println("Width: " + smallerBox.width());
+        System.out.println("Area: " + smallerBox.area() + " Volume: " + smallerBox.volume());
     }
 }
